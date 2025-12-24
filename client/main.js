4eaa146e479dcd7389a96b2d9cd5f61893496ffa
kekekuli <liuqiang01@travelsky.com.cn>
2025-12-24 09:53:06 +0800

    client could send graphql req

---
+++ client/main.js
@@ -0,0 +1,21 @@
+let dice = 3;
+let sides = 6;
+let query = /* GraphQL */ `
+  query RollDice($dice: Int!, $sides: Int) {
+    rollDice(numDice: $dice, numSides: $sides)
+  }
+`;
+
+fetch('http://localhost:4000/graphql', {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json',
+    Accept: 'application/json',
+  },
+  body: JSON.stringify({
+    query,
+    variables: { dice, sides },
+  }),
+})
+  .then((r) => r.json())
+  .then((data) => console.log('data returned:', data));
--- server/main.js
+++ server/main.js
@@ -1,20 +1,28 @@
-import { graphql, buildSchema } from 'graphql';
+import express from 'express';
+import { createHandler } from 'graphql-http/lib/use/express';
+import { buildSchema } from 'graphql';
 
 // Construct a schema, using GraphQL schema language
-const schema = buildSchema(`type Query { hello: String } `);
+const schema = buildSchema(`type Query { rollDice(numDice: Int!, numSides: Int): [Int] }`);
 
-// The rootValue provides a resolver function for each API endpoint
-const rootValue = {
-  hello() {
-    return 'Hello world!';
+// The root provides a resolver function for each API endpoint
+const root = {
+  rollDice({ numDice, numSides }) {
+    const output = [];
+    for (let i = 0; i < numDice; i++) {
+      output.push(1 + Math.floor(Math.random() * (numSides || 6)));
+    }
+    return output;
   },
 };
 
-// Run the GraphQL query '{ hello }' and print out the response
-graphql({
-  schema,
-  source: '{ hello }',
-  rootValue,
-}).then((response) => {
-  console.log(response);
-});
+const app = express();
+app.all(
+  '/graphql',
+  createHandler({
+    schema: schema,
+    rootValue: root,
+  }),
+);
+app.listen(4000);
+console.log('Running a GraphQL API server at localhost:4000/graphql');
